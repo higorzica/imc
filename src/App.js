2c097b0a8b427d70a4af75508101d071d3360134
Higor Zica <higorbhe@gmail.com>
2022-08-24 18:41:51 -0300

    Projeto Calculadora
    Tecnologia de desenvolvimento HTML, CSS e React

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import './app.css';
+import swal from 'sweetalert';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+export default function App() {
+  const [peso,setPeso] = useState('');
+  const [altura,setAltura] = useState('');
+
+  function calcularIMC() {
+    const alt = altura / 100;
+    const imc = peso / (alt * alt);
+    if (imc < 18.6) {
+      swal({title:"Atenção!",
+            text: "Você está abaixo do seu Peso! Seu IMC é : " + imc.toFixed(2),
+            icon: "success"});
+    }else if(imc >= 18.6 && imc < 24.9){
+      swal({title:"Atenção!",
+            text:"Você está no seu Peso Ideal! Seu IMC é : " + imc.toFixed(2),
+            icon:"success"});
+    }else if(imc >= 24.9 && imc < 34.9){
+      swal({title:"Atenção!",
+            text:"Você está levemente acima do seu Peso Ideal! Seu IMC é : " + imc.toFixed(2),
+            icon: "info"});
+    }else if(imc > 34.9){
+      swal({title:"Atenção!",
+            text:"Cuidado você está em estado de Obesidade! Seu IMC é : " + imc.toFixed(2),
+            icon: "info"});
+    }
+
+  }
+
+  return(
+    <div className="app">
+      <h1>Calculadora IMC</h1>
+      <span>Vamos calcular seu imc </span>
+
+      <div className="area-input">
+        <input type="text" placeholder="Peso em (kg) Ex:90 kg"
+        value={peso}
+        onChange={(e)=> setPeso(e.target.value)}>
+        </input>
+        <input type="text" placeholder="Altura em (cm) Ex:100 cm"
+        value={altura}
+        onChange={(e)=> setAltura(e.target.value)}>
+        </input>
+
+        <button onClick={calcularIMC}>
+          Calcular
+        </button>
+      </div>
+
+      <h2>
+        Vamos descobrir como anda sua Saúde !
+      </h2>
+
     </div>
-  );
-}
 
-export default App;
+    
+  )
+}
